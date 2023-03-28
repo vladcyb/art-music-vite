716c4a9cbe3a15e442da8f54e7c282426b463c5b
Vlad Tsibizov <vladtsibizov@yandex.ru>
2023-03-29 01:25:38 +0300

    Яндекс.Метрика, Яндекс.Вебмастер, vite-plugin-minify

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-
+import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteMinifyPlugin({}),
+  ],
   resolve: {
     alias: {
       '@components': resolve(__dirname, 'src/components'),
@@ -14,5 +17,5 @@ export default defineConfig({
       '@services': resolve(__dirname, 'src/services'),
       '@shared': resolve(__dirname, 'src/shared'),
     }
-  }
+  },
 })
